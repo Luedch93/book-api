273742b1a243387be75320906cf58f8a55ecd502
Luis Chavez <luischavezcald@gmail.com>
2020-05-23 13:41:58 -0400

    feat: create basic crud

---
+++ index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const app = require('./src/app')
+
+mongoose.connect('mongodb://localhost:27017')
+
+app.listen(4000, () => {
+  console.log(`Server running on port 4000`);
+})
---
+++ src/app.js
@@ -0,0 +1,31 @@
+const Express = require('express')
+const BookModel = require('./models/books')
+
+const app = Express()
+
+app.use(Express.json())
+
+app.get('/', async (req, res) => {
+  const books = await BookModel.find({})
+  res.send(books)
+})
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const book = await BookModel.findById(id)
+  res.send(book)
+})
+
+app.post('/', async (req, res) => {
+  const book = req.body
+  const response = await BookModel.create(book)
+  res.status(201).send(response)
+})
+
+app.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const book = await BookModel.findByIdAndDelete(id)
+  res.send(book)
+})
+
+module.exports = app
